b4326dec08444b56061c80ad9b806b477892b06b
pedrolucaspgb <pedro.lucas.2012@hotmail.com>
2024-08-11 22:13:41 -0300

    criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,6 @@
 class Pessoa:
+    olhos = 2
     def __init__(self,*filhos, nome=None, idade=35):
-
         self.nome= nome
         self.idade = idade
         self.filhos = list(filhos)
@@ -22,5 +22,12 @@ if __name__=="__main__":
         print(filho.nome)
     luciano.sobrenome = 'Silva'
     del luciano.filhos
+    luciano.olhos=1
+    del luciano.olhos
     print(luciano.__dict__)
     print(pedro.__dict__)
+    Pessoa.olhos=3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(pedro.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(pedro.olhos))
